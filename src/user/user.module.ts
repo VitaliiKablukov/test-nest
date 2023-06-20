6360a4f5c20e4512c43eb471d1d3df5ebf49acb1
VitaliiKablukov <vetal.kablukov98@gmail.com>
2023-06-20 17:37:21 +0300

    change setting cache

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -6,10 +6,16 @@ import { jwtConstants } from './constants';
 import { AuthMiddleware } from 'src/middlewares/authMiddlewares.service';
 import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { RedisClientOptions } from 'redis';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
-    CacheModule.register(),
+    CacheModule.register<RedisClientOptions>({
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+    }),
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
